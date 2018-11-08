c28be79a9b24db739c84b5d722960d6b15ba9696
Ubuntu <ubuntu@ip-172-31-46-110.us-west-2.compute.internal>
2018-11-08 01:10:49 +0000

    Update client.py

--- project_3/client.py
+++ project_3/client.py
@@ -1,5 +1,5 @@
 """
-ECE 196 Face Recognition Project
+CSE 196 Face Recognition Project
 Author: Will Chen, Simon Fong
 
 What this script should do:
@@ -13,11 +13,13 @@ import time,cv2, base64, requests
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
+people = ["bob", "rick", "sam", "david", "john", "sarah", "jane", "kristin", "will", "esther", "paul", "ryan", "joy", "jessica", "nick", "steve", "karina", "daniel", "janice"]
+
 # Font that will be written on the image
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 # TODO: Declare path to face cascade
-CASCADE_PATH = ""
+CASCADE_PATH = '/home/ubuntu/myFaceRecognition/project_2/haarcascade_frontalface_default.xml'
     
 def request_from_server(img):
     """ 
@@ -27,13 +29,13 @@ def request_from_server(img):
     :returns: Returns a dictionary containing label and cofidence.
     """
     # URL or PUBLIC DNS to your server
-    URL = ""
-    
+    URL = "ssh -i \"Part8Instance.pem\" ubuntu@ec2-54-245-187-228.us-west-2.compute.amazonaws.com"
+ 
     # File name so that it can be temporarily stored.
     temp_image_name = 'temp.jpg'
     
     # TODO: Save image with name stored in 'temp_image_name'
-    
+    cv2.imwrite('temp_image_name.jpg', temp_image_name);
 
     # Reopen image and encode in base64
     image = open(temp_image_name, 'rb') #open binary file in read mode
@@ -55,7 +57,7 @@ def request_from_server(img):
 def main():
     # 1. Start running the camera.
     # TODO: Initialize face detector
-    
+    face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
     
     # Initialize camera and update parameters
     camera = PiCamera()
@@ -73,41 +75,54 @@ def main():
     # 2. Detect a face, display it, and get confirmation from user.
     for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
         
-        # Get image array from frame
-        frame = frame.array
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Get image array from frame
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    frame = frame.array
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # TODO: Use face detector to get faces.
-        # Be sure to save the faces in a variable called 'faces'
+    # TODO: Use face detector to get faces.
+    # Be sure to save the faces in a variable called 'faces'
 
-        for (x, y, w, h) in faces:
-            print('==================================')
-            print('Face detected!')
-            cv2.imshow('Face Image for Classification', frame)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+	cv2.rectangle(faces,(x,y),(x+w,y+h),(255,255,255),2)
+           
+	faces = faces[y:y+h, x:x+w]
+
+	print('==================================')
+        print('Face detected!')
+        cv2.imshow('Face Image for Classification', frame)
             
-            # Keep showing image until a key is pressed
+        # Keep showing image until a key is pressed
+        cv2.waitKey()
+        answer = input('Confirm image (1-yes / 0-no): ')
+        print('==================================')
+
+        if(answer == 1):
+            print('Let\'s see who you are...')
+                
+            # TODO: Get label and confidence using request_from_server
+            predictions = request_from_server(faces)
+
+	    label = predictions['label']  
+	    confidence = predictions['confidence']  
+                
+            print('New result found!')
+
+            # TODO: Display label on face image
+            # Save what you want to write on image to 'result_to_display'
+            # [OPTIONAL]: At this point you only have a number to display, 
+            # you could add some extra code to convert your number to a 
+            # name
+
+	    person = people[label]
+
+            cv2.putText(frame, str("Hi " + person), (10, 30), FONT, 1, (0, 255, 0), 2)
+            cv2.imshow('Face Image for Classification', frame)
             cv2.waitKey()
-            answer = input('Confirm image (1-yes / 0-no): ')
-            print('==================================')
-
-            if(answer == 1):
-                print('Let\'s see who you are...')
-                
-                # TODO: Get label and confidence using request_from_server
-                
-                
-                print('New result found!')
-
-                # TODO: Display label on face image
-                # Save what you want to write on image to 'result_to_display'
-                # [OPTIONAL]: At this point you only have a number to display, 
-                # you could add some extra code to convert your number to a 
-                # name
-
-                cv2.putText(frame, str(result_to_display), (10, 30), FONT, 1, (0, 255, 0), 2)
-                cv2.imshow('Face Image for Classification', frame)
-                cv2.waitKey()
-                break
+            break
         
         # Delete image in variable so we can get the next frame
         rawCapture.truncate(0)
@@ -118,3 +133,4 @@ def main():
 # Runs main if this file is run directly
 if(__name__ == '__main__'):
     main()
+
